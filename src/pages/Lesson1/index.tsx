c3ecf09a14c2fd06ab9d0955fb357b402a3396a3
lind2760 <50762050+lind2760@users.noreply.github.com>
2022-12-15 20:31:03 +0800

    git提交规范管理 (#1)
    * fix: 临时存储
    
    * fix: git提交规范

---
+++ src/pages/Lesson1/index.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useMemo, useState } from "react";
+import { Input, Space } from "antd";
+import dayjs from "dayjs";
+
+function Lesson1() {
+  const [counter, setCounter] = useState(0);
+  const [timer, setTimer] = useState(dayjs());
+  useEffect(() => {
+    setInterval(() => {
+      setTimer(dayjs());
+    }, 1000);
+  }, []);
+
+  const todos = useMemo(() => {
+    console.log("进行了计算");
+    return counter;
+  }, [counter]);
+  return (
+    <Space
+      style={{
+        width: "100%",
+        height: 200,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h2>{dayjs(timer).format("YYYY-MM-DDTHH:mm:ss")}</h2>
+      <Space style={{ marginTop: 12 }}>
+        <Input
+          value={counter}
+          onChange={(e) => setCounter(Number(e.target.value))}
+        />
+      </Space>
+      <Space>输入框value：{todos}</Space>
+    </Space>
+  );
+}
+
+export default Lesson1;
